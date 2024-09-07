0927350bd7ce86c6de0a4b30268337fd4211e854
k-gourav <code2change.gourav@gmail.com>
2024-09-07 13:25:22 +0530

    Added accordion UI

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-import Accordion from '../components/Accordion'
+import star from "../assets/icons/icon-star.svg";
+import Accordion from "../components/Accordion/Accordion";
+import { accordionInformation } from "../utils/constants";
+
 
 const Home = () => {
-    return (
-        <Accordion />
-    )
-}
+  return (
+    <main>
+      <div className="accordion-container">
+        <div className="accordion-title">
+          <img className="title-icon" src={star} alt="star-icon" />
+          <h1 className="title-name">FAQs</h1>
+        </div>
+        </div>
+      {accordionInformation.map((element) => (
+        <Accordion key={element.id} description={element} />
+      ))}
+    </main>
+  );
+};
 
 export default Home;
---
+++ src/utils/constants.js
@@ -0,0 +1,33 @@
+export const accordionInformation = [
+  {
+    id: 1,
+    question: "What is Frontend Mentor, and how will it help me?",
+    answer:
+      "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+    collapseImg: "../assets/icons/icon-minus.svg",
+    expandImg: "../assets/icons/icon-plus.svg",
+  },
+  {
+    id: 2,
+    question: "Is Frontend Mentor free?",
+    answer:
+      "Yes, Frontend Mentor offers free learning platform to build projects.",
+    collapseImg: "../assets/icons/icon-minus.svg",
+    expandImg: "../assets/icons/icon-plus.svg",
+  },
+  {
+    id: 3,
+    question: "Can I use Frontend Mentor projects in my portfolio?",
+    answer: "Yes, You can use their projects in your portfolio to improve.",
+    collapseImg: "../assets/icons/icon-minus.svg",
+    expandImg: "../assets/icons/icon-plus.svg",
+  },
+  {
+    id: 4,
+    question: "How can I get help if I'm stuck on a challenge?",
+    answer:
+      "Post your queries on the community page regarding the challenge where you are stuck.",
+    collapseImg: "../assets/icons/icon-minus.svg",
+    expandImg: "../assets/icons/icon-plus.svg",
+  },
+];
