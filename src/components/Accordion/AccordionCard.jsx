f6f674c15fa00726abaef2275f1abc05f167fe6c
k-gourav <code2change.gourav@gmail.com>
2024-09-09 00:38:25 +0530

    feat : add expand/collapse functionality - Added collapse and expand feature to Accordion card component

--- src/components/Accordion/AccordionCard.jsx
+++ src/components/Accordion/AccordionCard.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./AccordionCard.module.css";
 import expandImg from "../../assets/icons/icon-plus.svg";
 import collapseImg from "../../assets/icons/icon-minus.svg";
 
 function AccordionCard({ description }) {
+  const [expandContent, setExpandContent] = useState(false);
+
   return (
     <div className={styles.accordion__body}>
       <div className={styles.accordion__content}>
         <div className={styles.accordion__title}>
-          <h3>{description.question}</h3>
-          <button className={styles.accordion__btn}>
-            <img src={expandImg} alt="expand-icon" />
+          <p className={styles.accordion_query}>{description.question}</p>
+          <button onClick={() => setExpandContent(!expandContent)} className={styles.accordion__btn}>
+            <img src={expandContent ? collapseImg : expandImg } alt={expandContent ? "collapse" : "expand"} />
           </button>
         </div>
-        <p className={styles.accordion__descript}>{description.answer}</p>
+        {expandContent ? <p className={styles.accordion__descript}>{description.answer}</p> : ""}
       </div>
     </div>
   );
